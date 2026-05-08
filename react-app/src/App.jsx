0361474d010fdade22068eb2c54d3b6d8fb69e9a
kogetumAru27 <tm10tora@icloud.com>
2026-05-08 16:20:46 +0900

    ポートフォリオ追加

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import Weather from "./weatherRea3";
+import Todo from "./todo8";
+import Constellation from "./Constellation3";
+import Stargame from "./star_game4";
 function App(){
+  const [tab,setTab] = useState("Todo");
   return(
-    <Weather />
+    <>
+    <nav>
+    <button onClick={() => setTab("Todo")}>Todo</button>
+    <button onClick={() => setTab("Stargame")}>星ゲーム</button>
+    <button onClick={() => setTab("Constellation")}>星座</button>
+    <button onClick={() => setTab("Weather")}>天気</button>
+    </nav>
+    <div>
+      {tab === "Todo" && <Todo/>}
+      {tab === "Stargame" && <Stargame/>}
+      {tab === "Constellation" && <Constellation/>}
+      {tab === "Weather" && <Weather/>}
+    </div>
+    </>
   )
 }
 export default App;
